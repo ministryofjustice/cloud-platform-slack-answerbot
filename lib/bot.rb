d09bd3b008898cc606d3f8b356eb01972bc44e62
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2019-10-18 17:17:07 +0100

    clean bot.rb and format

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,36 +4,33 @@ module AnswerBot
   class App < SlackRubyBot::Bot
     
     scan(/([a-zA-Z0-9]*)/) do |client, data, input|
-      # keywords.each do |search|
-      #   begin
-          # search_link = @@parser.search(keywords)
-          return if (!SlackRubyBot::Config.allow_message_loops?) && (client.self && client.self.id == data.user)
-          input = data.text
-          keyword_list = {
-            ["user guide"] => 
-              "https://user-guide.cloud-platform.service.justice.gov.uk",
-            ["resource limits", "namespace request limits", "usage report"] => 
-              "https://user-guide.cloud-platform.service.justice.gov.uk/concepts.html#namespace-container-resource-limits",
-            ["template deploy", "DNS changes", "AWS access"] => 
-              "https://user-guide.cloud-platform.service.justice.gov.uk/concepts.html#migrating-to-the-cloud-platform-from-template-deploy",
-            ["kubectl installation"] => 
-              "https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#installation",
-            ["authenticate"] => 
-              "https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#authentication",
-            ["migrate RDS", "RDS instance"] => 
-              "https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#migrating-an-rds-instance",
-            ["grafana dashboard", "prometheus metrics", "custom alerts"] => 
-              "https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#monitoring-applications"
-          }
-    
-          keyword_list.each do |list, href|
-            list.each do |keyword|
-              if input.include?(keyword)
-                puts "Keyword match #{keyword}"
-                client.say(channel: data.channel, text: "User guide: #{href}")
-              end
-            end
+      return if (!SlackRubyBot::Config.allow_message_loops?) && (client.self && client.self.id == data.user)
+      input = data.text
+      keyword_list = {
+        ["user guide"] => 
+          "https://user-guide.cloud-platform.service.justice.gov.uk",
+        ["resource limits", "namespace request limits", "usage report"] => 
+          "https://user-guide.cloud-platform.service.justice.gov.uk/concepts.html#namespace-container-resource-limits",
+        ["template deploy", "DNS changes", "AWS access"] => 
+          "https://user-guide.cloud-platform.service.justice.gov.uk/concepts.html#migrating-to-the-cloud-platform-from-template-deploy",
+        ["kubectl installation"] => 
+          "https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#installation",
+        ["authenticate"] => 
+          "https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#authentication",
+        ["migrate RDS", "RDS instance"] => 
+          "https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#migrating-an-rds-instance",
+        ["grafana dashboard", "prometheus metrics", "custom alerts"] => 
+          "https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#monitoring-applications"
+      }
+
+      keyword_list.each do |list, href|
+        list.each do |keyword|
+          if input.include?(keyword)
+            puts "Keyword match #{keyword}"
+            client.say(channel: data.channel, text: "User guide: #{href}")
           end
+        end
+      end
 
     end
 
@@ -43,7 +40,7 @@ module AnswerBot
     match /^(?<bot>\w*)$/
 
     def self.call(client, data, _match)
-      client.say(channel: data.channel, text: "Uhhh")
+      client.say(channel: data.channel, text: "hello")
     end
   end
 end
